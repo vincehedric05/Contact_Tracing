5b0b59de8737ce5d0112f1fa9eb36512932e9967
vincehedric05 <82695796+vincehedric05@users.noreply.github.com>
2021-06-03 20:18:46 +0800

    commit

---
+++ Contact Tracing/Form1.Designer.cs
@@ -0,0 +1,266 @@
+﻿
+namespace Contact_Tracing
+{
+    partial class main
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lastName_txtbox = new System.Windows.Forms.TextBox();
+            this.address_txtbox = new System.Windows.Forms.TextBox();
+            this.lastName_label = new System.Windows.Forms.Label();
+            this.address_label = new System.Windows.Forms.Label();
+            this.age_txtbox = new System.Windows.Forms.TextBox();
+            this.contactNumber_txtbox = new System.Windows.Forms.TextBox();
+            this.age_label = new System.Windows.Forms.Label();
+            this.contactNumber_label = new System.Windows.Forms.Label();
+            this.confirm_btn = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.firstName_txtbox = new System.Windows.Forms.TextBox();
+            this.firstName_label = new System.Windows.Forms.Label();
+            this.male_radiobtn = new System.Windows.Forms.RadioButton();
+            this.female_radiobtn = new System.Windows.Forms.RadioButton();
+            this.sex_label = new System.Windows.Forms.Label();
+            this.data_btn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lastName_txtbox
+            // 
+            this.lastName_txtbox.Location = new System.Drawing.Point(12, 140);
+            this.lastName_txtbox.Name = "lastName_txtbox";
+            this.lastName_txtbox.Size = new System.Drawing.Size(188, 23);
+            this.lastName_txtbox.TabIndex = 0;
+            this.lastName_txtbox.TabStop = false;
+            // 
+            // address_txtbox
+            // 
+            this.address_txtbox.Location = new System.Drawing.Point(12, 189);
+            this.address_txtbox.Name = "address_txtbox";
+            this.address_txtbox.Size = new System.Drawing.Size(188, 23);
+            this.address_txtbox.TabIndex = 1;
+            this.address_txtbox.TabStop = false;
+            // 
+            // lastName_label
+            // 
+            this.lastName_label.AutoSize = true;
+            this.lastName_label.Font = new System.Drawing.Font("Agency FB", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lastName_label.Location = new System.Drawing.Point(12, 117);
+            this.lastName_label.Name = "lastName_label";
+            this.lastName_label.Size = new System.Drawing.Size(63, 20);
+            this.lastName_label.TabIndex = 2;
+            this.lastName_label.Text = "Last Name ";
+            // 
+            // address_label
+            // 
+            this.address_label.AutoSize = true;
+            this.address_label.Font = new System.Drawing.Font("Agency FB", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.address_label.Location = new System.Drawing.Point(12, 166);
+            this.address_label.Name = "address_label";
+            this.address_label.Size = new System.Drawing.Size(51, 20);
+            this.address_label.TabIndex = 3;
+            this.address_label.Text = "Address";
+            // 
+            // age_txtbox
+            // 
+            this.age_txtbox.Location = new System.Drawing.Point(209, 140);
+            this.age_txtbox.Name = "age_txtbox";
+            this.age_txtbox.Size = new System.Drawing.Size(110, 23);
+            this.age_txtbox.TabIndex = 4;
+            this.age_txtbox.TabStop = false;
+            // 
+            // contactNumber_txtbox
+            // 
+            this.contactNumber_txtbox.Location = new System.Drawing.Point(209, 189);
+            this.contactNumber_txtbox.Name = "contactNumber_txtbox";
+            this.contactNumber_txtbox.Size = new System.Drawing.Size(110, 23);
+            this.contactNumber_txtbox.TabIndex = 5;
+            this.contactNumber_txtbox.TabStop = false;
+            // 
+            // age_label
+            // 
+            this.age_label.AutoSize = true;
+            this.age_label.Font = new System.Drawing.Font("Agency FB", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.age_label.Location = new System.Drawing.Point(209, 117);
+            this.age_label.Name = "age_label";
+            this.age_label.Size = new System.Drawing.Size(28, 20);
+            this.age_label.TabIndex = 6;
+            this.age_label.Text = "Age";
+            // 
+            // contactNumber_label
+            // 
+            this.contactNumber_label.AutoSize = true;
+            this.contactNumber_label.Font = new System.Drawing.Font("Agency FB", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.contactNumber_label.Location = new System.Drawing.Point(209, 166);
+            this.contactNumber_label.Name = "contactNumber_label";
+            this.contactNumber_label.Size = new System.Drawing.Size(88, 20);
+            this.contactNumber_label.TabIndex = 7;
+            this.contactNumber_label.Text = "Contact Number";
+            // 
+            // confirm_btn
+            // 
+            this.confirm_btn.Font = new System.Drawing.Font("Agency FB", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.confirm_btn.Location = new System.Drawing.Point(12, 218);
+            this.confirm_btn.Name = "confirm_btn";
+            this.confirm_btn.Size = new System.Drawing.Size(307, 39);
+            this.confirm_btn.TabIndex = 8;
+            this.confirm_btn.TabStop = false;
+            this.confirm_btn.Text = "Confirm";
+            this.confirm_btn.UseVisualStyleBackColor = true;
+            this.confirm_btn.Click += new System.EventHandler(this.confirm_click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Agency FB", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label5.Location = new System.Drawing.Point(98, 9);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(148, 34);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Contact Tracing";
+            // 
+            // firstName_txtbox
+            // 
+            this.firstName_txtbox.Location = new System.Drawing.Point(12, 91);
+            this.firstName_txtbox.Name = "firstName_txtbox";
+            this.firstName_txtbox.Size = new System.Drawing.Size(188, 23);
+            this.firstName_txtbox.TabIndex = 10;
+            this.firstName_txtbox.TabStop = false;
+            // 
+            // firstName_label
+            // 
+            this.firstName_label.AutoSize = true;
+            this.firstName_label.Font = new System.Drawing.Font("Agency FB", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.firstName_label.Location = new System.Drawing.Point(12, 68);
+            this.firstName_label.Name = "firstName_label";
+            this.firstName_label.Size = new System.Drawing.Size(65, 20);
+            this.firstName_label.TabIndex = 11;
+            this.firstName_label.Text = "First Name ";
+            // 
+            // male_radiobtn
+            // 
+            this.male_radiobtn.AutoSize = true;
+            this.male_radiobtn.Font = new System.Drawing.Font("Agency FB", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.male_radiobtn.Location = new System.Drawing.Point(223, 89);
+            this.male_radiobtn.Name = "male_radiobtn";
+            this.male_radiobtn.Size = new System.Drawing.Size(35, 24);
+            this.male_radiobtn.TabIndex = 12;
+            this.male_radiobtn.Text = "M";
+            this.male_radiobtn.UseVisualStyleBackColor = true;
+            // 
+            // female_radiobtn
+            // 
+            this.female_radiobtn.AutoSize = true;
+            this.female_radiobtn.Font = new System.Drawing.Font("Agency FB", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.female_radiobtn.Location = new System.Drawing.Point(265, 90);
+            this.female_radiobtn.Name = "female_radiobtn";
+            this.female_radiobtn.Size = new System.Drawing.Size(32, 24);
+            this.female_radiobtn.TabIndex = 13;
+            this.female_radiobtn.Text = "F";
+            this.female_radiobtn.UseVisualStyleBackColor = true;
+            // 
+            // sex_label
+            // 
+            this.sex_label.AutoSize = true;
+            this.sex_label.Font = new System.Drawing.Font("Agency FB", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.sex_label.Location = new System.Drawing.Point(209, 66);
+            this.sex_label.Name = "sex_label";
+            this.sex_label.Size = new System.Drawing.Size(65, 20);
+            this.sex_label.TabIndex = 14;
+            this.sex_label.Text = "First Name ";
+            // 
+            // data_btn
+            // 
+            this.data_btn.Font = new System.Drawing.Font("Agency FB", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.data_btn.Location = new System.Drawing.Point(12, 302);
+            this.data_btn.Name = "data_btn";
+            this.data_btn.Size = new System.Drawing.Size(307, 39);
+            this.data_btn.TabIndex = 15;
+            this.data_btn.TabStop = false;
+            this.data_btn.Text = "Open";
+            this.data_btn.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Agency FB", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(12, 279);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 20);
+            this.label1.TabIndex = 16;
+            this.label1.Text = "Data File";
+            // 
+            // main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(330, 353);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.data_btn);
+            this.Controls.Add(this.sex_label);
+            this.Controls.Add(this.female_radiobtn);
+            this.Controls.Add(this.male_radiobtn);
+            this.Controls.Add(this.firstName_label);
+            this.Controls.Add(this.firstName_txtbox);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.confirm_btn);
+            this.Controls.Add(this.contactNumber_label);
+            this.Controls.Add(this.age_label);
+            this.Controls.Add(this.contactNumber_txtbox);
+            this.Controls.Add(this.age_txtbox);
+            this.Controls.Add(this.address_label);
+            this.Controls.Add(this.lastName_label);
+            this.Controls.Add(this.address_txtbox);
+            this.Controls.Add(this.lastName_txtbox);
+            this.Name = "main";
+            this.Text = "9+";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox lastName_txtbox;
+        private System.Windows.Forms.TextBox address_txtbox;
+        private System.Windows.Forms.Label lastName_label;
+        private System.Windows.Forms.Label address_label;
+        private System.Windows.Forms.TextBox age_txtbox;
+        private System.Windows.Forms.TextBox contactNumber_txtbox;
+        private System.Windows.Forms.Label age_label;
+        private System.Windows.Forms.Label contactNumber_label;
+        private System.Windows.Forms.Button confirm_btn;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox firstName_txtbox;
+        private System.Windows.Forms.Label firstName_label;
+        private System.Windows.Forms.RadioButton male_radiobtn;
+        private System.Windows.Forms.RadioButton female_radiobtn;
+        private System.Windows.Forms.Label sex_label;
+        private System.Windows.Forms.Button data_btn;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Contact Tracing/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Contact_Tracing
+{
+    public partial class main : Form
+    {
+        public main()
+        {
+            InitializeComponent();
+        }
+
+        private void confirm_click(object sender, EventArgs e)
+        {
+            StreamWriter outputFile;
+            outputFile = File.CreateText("data.txt");
+            outputFile.WriteLine(firstName_txtbox.Text);
+            outputFile.Close();
+        }
+    }
+}
